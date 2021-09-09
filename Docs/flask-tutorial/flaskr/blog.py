b1a4c37a06cd1f2bbfd35cdc6f74bfc9e60bab70
Vikas Viki <vikasviki489@gmail.com>
2021-09-09 21:23:25 +0530

    Test Coverage - Blog

--- Docs/flask-tutorial/flaskr/blog.py
+++ Docs/flask-tutorial/flaskr/blog.py
@@ -19,6 +19,7 @@ def index():
     return render_template('blog/index.html', posts=posts)
 
 @bp.route('/create', methods=('GET', 'POST'))
+@login_required
 def create():
     if request.method == 'POST':
         title = request.form['title']
